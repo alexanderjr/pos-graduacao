cd108e5e31395b047c082c3782722bd2b58d3090
Alex <aafonso.93@gmail.com>
2018-05-22 16:39:36 -0300

    Linear regression

--- SMA/atv_previa.py
+++ SMA/atv_previa.py
@@ -1,89 +1,55 @@
 from sklearn.datasets import load_boston
 from sklearn import datasets, linear_model
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, mean_absolute_error, median_absolute_error
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import sys
 
-#https://gist.github.com/sanguedemonstro/53071059def69958a97098c41e2e0e51
-
 def clear():
     sys.stderr.write("\x1b[2J\x1b[H")
 
-def summary(data):
-    data_frame = pd.DataFrame(data.data, columns=data.feature_names)
-    target = pd.DataFrame(data.target, columns=["MEDV"])
-
-    x = data_frame["RM"]
-    y = target["MEDV"]
-
-    model = sm.OLS(y,x).fit()
-    predictions = model.predict(x)
-
-    model.summary()
-
 def linear_regression(data):
 
-    # #get all data from specific position data and features
-    # features = [data.feature_names[0], data.feature_names[5]]
-    # feature_data = data.data[0:, [0,5]]
-
-    # #dataframe with values and columns
-    # df = pd.DataFrame(feature_data, columns=features)
-    # #df = pd.DataFrame(data.data, columns=data.feature_names)
-    
-    # #dataframe with target values
-    # target = pd.DataFrame(data.target, columns=["MEDV"])
-
-    # #instance of LinearRegression ML
-    # lm = linear_model.LinearRegression()
-    
-    # #fit our model
-    # model = lm.fit(df,target["MEDV"])
-    
-    # #make predictions
-    # predictions = lm.predict(df)
-
-    # print "SCORE:"
-    # print (lm.score(df, target["MEDV"]))
-    # print "COEFICIENTE :"
-    # print (lm.coef_)
-    
-    diabetes = datasets.load_diabetes()
-
-    print diabetes.feature_names
-    diabetes_X = diabetes.data[:, np.newaxis, 2]
-    print diabetes_X
-
-    exit()
-
-    data_X = data.data[:, np.newaxis,2]
-
-    print len(data_X)
-
-    exit()
+    #get only one feature (in this case number of rooms)
+    data_x = data.data[:, np.newaxis, 5]
 
     qtd_train = 350
 
-    x_train = data.data[0:qtd_train + 1]
-    x_test  = data.data[qtd_train + 1:]
+    #split data in train and test
+    x_train = data_x[0:qtd_train + 1]
+    x_test  = data_x[qtd_train + 1:]
 
     y_train = data.target[0:qtd_train + 1]
     y_test  = data.target[qtd_train + 1:]
+    
+    #creating model
+    regr = linear_model.LinearRegression()
+    regr.fit(x_train, y_train)
 
-    regr = linear_model.LinearRegression
+    y_predictions = regr.predict(x_test)
+
+    #print("FEATURE ANALISADA: %s" % feature)
+    print('Coefficients: \n', regr.coef_)
+
+    print("ERRO MEDIO QUADRADO: %.2f" % mean_squared_error(y_test, y_predictions))
+    print("ERRO MEDIO ABSOLUTO: %.2f" % mean_absolute_error(y_test, y_predictions))
+    print("ERRO MEDIANO ABSOLUTO: %.2f" % median_absolute_error(y_test, y_predictions))
+    print("\n")
+    #count = count + 1
+
+    # Plot outputs
+    plt.scatter(x_test, y_test,  color='black')
+    plt.plot(x_test, y_predictions, color='blue', linewidth=3)
+    plt.xlabel("QTD MEDIA DOS QUARTOS")
+    plt.ylabel("PRECO MEDIO DAS CASAS")
+
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
     
 
 linear_regression(load_boston())
-
-# 1 - Escolher uma feature para avaliar coeficiente de determinação, a fim de verificar se existe uma relação entrea a variável de entrada com a de saída (target)
-
-# 2 - Após avaliar, escolher
-
-#https://towardsdatascience.com/simple-and-multiple-linear-regression-in-python-c928425168f9
-
-#https://towardsdatascience.com/a-quick-introduction-to-the-pandas-python-library-f1b678f34673
-
-#http://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html
