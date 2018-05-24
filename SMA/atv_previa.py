e79bba57be17b187cdc1555a83615a3777612389
Alex <aafonso.93@gmail.com>
2018-05-24 11:21:39 -0300

    Final

--- SMA/atv_previa.py
+++ SMA/atv_previa.py
@@ -3,6 +3,7 @@ from sklearn import datasets, linear_model
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, mean_absolute_error, median_absolute_error
 from matplotlib import pyplot as plt
+from sklearn.model_selection import cross_val_predict
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
@@ -11,7 +12,19 @@ import sys
 def clear():
     sys.stderr.write("\x1b[2J\x1b[H")
 
-def linear_regression(data, index, feature_name):
+def metrics(test, predictions):
+    return_mean_squared_error = mean_squared_error(test, predictions)
+    return_mean_absolute_error = mean_absolute_error(test, predictions)
+    return_median_absolute_error = median_absolute_error(test, predictions)
+
+    print("ERRO MEDIO QUADRADO: %.2f" % return_mean_squared_error)
+    print("ERRO MEDIO ABSOLUTO: %.2f" % return_mean_absolute_error)
+    print("ERRO MEDIANO ABSOLUTO: %.2f" % return_median_absolute_error)
+    print("\n")
+
+    return mean_squared_error, mean_absolute_error, median_absolute_error
+
+def linear_regression_simple(data, index, feature_name):
 
     #get only one feature (in this case number of rooms)
     data_x = data.data[:, np.newaxis, index]
@@ -32,35 +45,15 @@ def linear_regression(data, index, feature_name):
     y_predictions = regr.predict(x_test)
 
     print("FEATURE ANALISADA: %s" % feature_name)
-    #print('Coefficients: \n', regr.coef_)
-    print regr.score(x_test, y_test)
     
-    return_mean_squared_error = mean_squared_error(y_test, y_predictions)
-    return_mean_absolute_error = mean_absolute_error(y_test, y_predictions)
-    return_median_absolute_error = median_absolute_error(y_test, y_predictions)
+    metrics(y_test, y_predictions)
 
-    print("ERRO MEDIO QUADRADO: %.2f" % return_mean_squared_error)
-    print("ERRO MEDIO ABSOLUTO: %.2f" % return_mean_absolute_error)
-    print("ERRO MEDIANO ABSOLUTO: %.2f" % return_median_absolute_error)
-    print("\n")
-    #count = count + 1
-
-    return mean_squared_error, mean_absolute_error, median_absolute_error
-
-    # # Plot outputs
-    # plt.scatter(x_test, y_test,  color='black')
-    # plt.plot(x_test, y_predictions, color='blue', linewidth=3)
-    # plt.xlabel("QTD MEDIA DOS QUARTOS")
-    # plt.ylabel("PRECO MEDIO DAS CASAS")
-
-    # plt.xticks(())
-    # plt.yticks(())
-
-    # plt.show()
 
 data = load_boston()
 
+#EXECUTANDO REGRESSÃO LINEAR SIMPLES
+print ("Regressão Simples")
 count = 0
 for feature in data.feature_names:
-    linear_regression(data, count, feature)
+    linear_regression_simple(data, count, feature)
     count = count + 1
