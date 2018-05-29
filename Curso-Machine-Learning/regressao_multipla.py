87109e87e3756a7c4a748f53d97b4831e12d34da
alexander <alex@agenciamanga.com>
2018-05-29 07:38:33 -0300

    Regressão linear

---
+++ Curso-Machine-Learning/regressao_multipla.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, median_absolute_error, mean_absolute_error
+
+movies = pd.read_csv("datasets/movies_multilinear_reg.csv")
+
+filmes_independentes = movies[movies.columns[2:17]]
+filmes_dependentes = movies[movies.columns[17:]]
+
+X_train, X_test, Y_train, Y_test = train_test_split(filmes_independentes, filmes_dependentes, test_size=0.3)
+
+X_train = np.array(X_train).reshape(len(X_train), 15)
+X_test = np.array(X_test).reshape(len(X_test), 15)
+
+modelo = LinearRegression()
+modelo.fit(X_train, Y_train)
+
+Y_predict = modelo.predict(X_test)
+
+# print "Erro médio Absoluto"
+# print modelo.score(X_test, Y_predict)
+
+# print "\n"
+
+print "Coeficiente de Determinação"
+print r2_score(Y_test, Y_predict)
+
+print "\n"
+
+print "Erro médio absoluto"
+print mean_absolute_error(Y_test, Y_predict)
+
+print "\n"
+
+print "Erro mediano absoluto"
+print median_absolute_error(Y_test, Y_predict)
+
+print "\n"
+
+print "Erro médio quadrado"
+print mean_squared_error(Y_test, Y_predict)
+
+
+
